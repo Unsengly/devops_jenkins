71ef33ff5042c898fdeed374242f3bfa4cb94bc3
Lily <unsengly.e20201382@gmail.com>
2024-03-20 17:52:09 +0700

    make a mistake to test Email Notification

--- src/main/java/project/jenkins/App.java
+++ src/main/java/project/jenkins/App.java
@@ -3,5 +3,6 @@ package project.jenkins;
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("Hellddsdsddo world!")
     }
 }
